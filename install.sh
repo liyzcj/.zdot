31e425b9ad03570e07c9cb330fb0e5cb1df379e7
liyzcj <liyz0912@gmail.com>
2018-08-01 16:21:04 +0300

    add install.sh

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#####################################################################################
+# This script installs the dotfiles and runs all other system configuration scripts #
+# @author Li Yanzhe, 2018.                                                          #
+#####################################################################################
+
+source ./lib_sh/echoflags.sh
+
+bot "Hi! I'm going to install tooling and tweak your system settings. Here I go..."
+
