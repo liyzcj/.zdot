473a754b6beb039d14423bdf9a971271af7d0b64
liyzcj <liyz0912@gmail.com>
2018-08-02 11:19:52 +0300

    implement install packages

--- install.sh
+++ install.sh
@@ -8,5 +8,17 @@
 source ./lib_sh/echoflags.sh
 
 bot "Hi! I'm going to install tooling and tweak your system settings. Here I go..."
-action "Installing required packages"
 
+## Install required packages ########################################################
+action "Installing required packages"
+for pac in $(sed 's/#.*//' ./requirement)
+do
+	running "Installing $COL_CYAN$pac"
+	sudo apt-get -y install $pac >/dev/null 2>&1
+	if [ $? == 0 ]
+	then
+		ok "Successful!"
+	else
+		error "Failed!"
+	fi
+done
